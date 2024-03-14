7b29ada0fef914157938a1884b00bf4c057a0e8e
Galih Prayoga <galihp83@gmail.com>
2024-03-14 23:44:42 +0700

    create chart (not fixed)

--- features/charts/entity.go
+++ features/charts/entity.go
@@ -1,1 +1,51 @@
 package charts
+
+import (
+	"context"
+	"io"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Chart struct {
+	ID    string
+	Total float64
+
+	UserId  uint
+	Details []ChartDetail
+}
+
+type ChartDetail struct {
+	Quantity int
+	Subtotal float64
+
+	ChartId  string
+	Products Product
+}
+
+type Product struct {
+	ID        uint
+	Name      string
+	Price     float64
+	Thumbnail Image
+
+	UserId uint
+}
+
+type Image struct {
+	ID       uint
+	ImageURL string
+	ImageRaw io.Reader
+}
+
+type Handler interface {
+	Create() echo.HandlerFunc
+}
+
+type Service interface {
+	Create(ctx context.Context, data Chart) error
+}
+
+type Repository interface {
+	Create(ctx context.Context, data Chart) error
+}
--- features/charts/repository/repository.go
+++ features/charts/repository/repository.go
@@ -1,1 +1,61 @@
 package repository
+
+import (
+	"bilo/features/charts"
+	"context"
+
+	"gorm.io/gorm"
+)
+
+type Chart struct {
+	Id    string  `gorm:"column:id; primaryKey;"`
+	Total float64 `gorm:"column:total; type:decimal(16,2);"`
+
+	UserId uint `gorm:"column:user_id;"`
+	User   User `gorm:"foreignKey:UserId"`
+
+	Details []ChartDetail
+}
+
+type ChartDetail struct {
+	Quantity int     `gorm:"column:quantity; type:integer;"`
+	Subtotal float64 `gorm:"column:subtotal; type:decimal(16,2);"`
+
+	ChartId string `gorm:"column:chart_id;"`
+	Chart   Chart  `gorm:"foreignKey:ChartId;"`
+
+	ProductId uint    `gorm:"column:product_id;"`
+	Product   Product `gorm:"foreignKey:ProductId;"`
+}
+
+type User struct {
+	Id uint
+}
+
+type Product struct {
+	Id           uint
+	Name         string
+	Price        float64
+	ThumbnailUrl string
+
+	UserId uint `gorm:"column:user_id;"`
+	User   User `gorm:"foreignKey:UserId"`
+}
+
+type chartRepository struct {
+	db *gorm.DB
+}
+
+func NewChartRepository(db *gorm.DB) charts.Repository {
+	return &chartRepository{
+		db: db,
+	}
+}
+
+func (repo *chartRepository) Create(ctx context.Context, data charts.Chart) error {
+	var inputDB = new(Chart)
+
+	inputDB.UserId = data.UserId
+
+	return nil
+}

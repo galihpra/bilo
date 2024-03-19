ad8ba28faae6ae2ccaad10cb18230716aa13068c
Galih Prayoga <galihp83@gmail.com>
2024-03-19 22:55:53 +0700

    charts to chart

--- features/charts/handler/request.go
+++ features/charts/handler/request.go
@@ -2,10 +2,10 @@ package handler
 
 type CreateRequest struct {
 	UserId    uint
-	ProductId []Product
+	ProductId Product
 }
 
 type Product struct {
 	ProductId uint `json:"product_id" form:"product_id"`
-	Qty       uint `json:"qty" form:"qty"`
+	Qty       int  `json:"qty" form:"qty"`
 }
